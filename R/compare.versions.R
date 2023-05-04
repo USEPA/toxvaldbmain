b759e0e5de24fcf879718fbc48f362118573058d
richardjudson <judson.richard@epa.gov>
2023-05-04 08:25:27 -0400

    initial commit

---
+++ R/compare.versions.R
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------------
+#' Compare versions of toxval
+#' @param db1 The old version of the database
+#' @param db2 The new version of the database
+#' @export
+#--------------------------------------------------------------------------------------
+compare.versions <- function(db1="res_toxval_v94",db2="res_toxval_v95") {
+  printCurrentFunction(paste(db1,":", db2))
+  slist = runQuery("select distinct source from toxval",db1)[,1]
+  for(source in slist) {
+    n1 = runQuery(paste0("select count(*) from toxval where source='",source,"'"),db1)[,1]
+    n2 = runQuery(paste0("select count(*) from toxval where source='",source,"'"),db2)[,1]
+    if(n1!=n2) cat(source,n1,n2,"\n")
+  }
+}
