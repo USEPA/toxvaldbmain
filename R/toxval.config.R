deccc2d58fdf3578f558ecb7deab70c6ae751abc
Taylor Wall <45834109+taywll252@users.noreply.github.com>
2023-06-06 08:52:22 -0400

    HotFix Update toxval.config.R
    HotFix added "non_hashing_cols"

--- R/toxval.config.R
+++ R/toxval.config.R
@@ -36,11 +36,15 @@ toxval.config <- function() {
   datapath = "/ccte/ACToR1/ToxValDB9/Repo/"
   actorws.prod <- "https://actorws.epa.gov/actorws/toxval/v01/toxval_source"
   actorws.dev <- "http://ag.epa.gov:8528/actorws/toxval/v01/toxval_source"
+  non_hash_cols = c("chemical_id", "parent_chemical_id", "source_id","clowder_id","document_name","source_hash","qc_status",
+                    "parent_hash","create_time","modify_time","created_by", "qc_flags", "qc_notes", "version",
+                    "raw_input_file", "source_version_date")
   retval <- list(dsstox.db=dsstox.db,
               toxval.db=toxval.db,
               source.db=source.db,
               datapath=datapath,
               actorws.prod=actorws.prod,
-              actorws.dev=actorws.dev)
+              actorws.dev=actorws.dev,
+              non_hash_cols=non_hash_cols)
   retval
 }
