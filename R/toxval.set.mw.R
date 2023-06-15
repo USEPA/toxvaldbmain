911b045344aeb56e79438e0c2c73805fc284330b
Taylor Wall <wall.jonathan@epa.gov>
2023-06-15 12:52:13 -0400

    Updates to use CCTE Chemicals API

--- R/toxval.set.mw.R
+++ R/toxval.set.mw.R
@@ -11,31 +11,63 @@ toxval.set.mw <- function(toxval.db, source=NULL){
   } else {
     slist = runQuery("select distinct source from toxval",toxval.db)[,1]
   }
-
+  # Loop through each source
   for(source in slist) {
     cat(source,"\n")
     runQuery(paste0("update toxval set mw=-1 where mw is null and source='",source,"'"),toxval.db)
+    # Pull list of DTXSID values as a vector
     dlist = runQuery(paste0("select distinct dtxsid from toxval where source='",source,"' and mw<0"),toxval.db)[,1]
 
-    # Query DSSTox once for source dlist
-    query = paste0(
-      "select ",
-      dsstox.db,".generic_substances.dsstox_substance_id as dtxsid, ",
-      dsstox.db,".compounds.mol_weight as mw
+    # Use bulk DTXSID CCTE Chemicals API pull (limit 200 per call)
+    if(!is.null(API_AUTH)){
+      cat("...Pulling DSSTox mw using CCTE API...\n")
+      # Split list into subsets of 200
+      mw <- dlist %>%
+        split(., rep(1:ceiling(length(.)/200), each=200, length.out=length(.)))
+      # Loop through the groups of 200 DTXSID values
+      for(i in seq_len(length(mw))){
+        # Wait between calls for API courtesy
+        Sys.sleep(0.25)
+        cat("...Pulling DSSTox mw ", i , " of ", length(mw), "\n")
+        mw[[i]] <- httr::POST(
+          "https://api-ccte.epa.gov/chemical/detail/search/by-dtxsid/",
+          httr::accept_json(),
+          httr::content_type_json(),
+          # Use API Key for authorization
+          httr::add_headers(`x-api-key` = API_AUTH),
+          encode = "json",
+          body=as.list(mw[[i]])
+        ) %>%
+          httr::content() %>%
+          dplyr::bind_rows() %>%
+          dplyr::select(dtxsid, mw=averageMass)
+      }
+      # Combine all results
+      mw = dplyr::bind_rows(mw)
+    } else {
+      cat("...Pulling DSSTox mw using DSSTox Database...\n")
+      # Access directly from DSSTox (if user has SELECT access)
+      # Query DSSTox once for source dlist
+      query = paste0(
+        "select ",
+        dsstox.db,".generic_substances.dsstox_substance_id as dtxsid, ",
+        dsstox.db,".compounds.mol_weight as mw
         from ",
-      dsstox.db,".compounds, ",
-      dsstox.db,".generic_substance_compounds, ",
-      dsstox.db,".generic_substances
+        dsstox.db,".compounds, ",
+        dsstox.db,".generic_substance_compounds, ",
+        dsstox.db,".generic_substances
         where ",
-      dsstox.db,".compounds.id = ",dsstox.db,".generic_substance_compounds.fk_compound_id
+        dsstox.db,".compounds.id = ",dsstox.db,".generic_substance_compounds.fk_compound_id
         and ",
-      dsstox.db,".generic_substance_compounds.fk_generic_substance_id = ",dsstox.db,".generic_substances.id
+        dsstox.db,".generic_substance_compounds.fk_generic_substance_id = ",dsstox.db,".generic_substances.id
         and ",dsstox.db,".generic_substances.dsstox_substance_id IN ('",
-      paste0(dlist, collapse = "', '"),
-      "')"
-    )
-    mw = runQuery(query,toxval.db) %>%
-      filter(!is.na(mw))
+        paste0(dlist, collapse = "', '"),
+        "')"
+      )
+      mw = runQuery(query,toxval.db) %>%
+        filter(!is.na(mw))
+    }
+
     # Only run if mw values are present for selected DTXSID values
     if(nrow(mw)){
       # Query to inner join and make updates with mw dataframe (temp table added/dropped)
