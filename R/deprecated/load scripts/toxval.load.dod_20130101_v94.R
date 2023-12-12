47ca5d538a010d79bc44a8b74432a55b87523f47
Winter <Winter.Samuel@epa.gov>
2023-12-12 10:05:24 -0500

    Updated DOD MEG load script logic and removed import script transformations

--- R/deprecated/load scripts/toxval.load.dod_20130101_v94.R
+++ R/deprecated/load scripts/toxval.load.dod_20130101_v94.R
@@ -5,8 +5,13 @@
 #' @param log If TRUE, send output to a log file
 #' @export
 #--------------------------------------------------------------------------------------
-toxval.load.dod <- function(toxvaldb,source.db, log=FALSE, remove_null_dtxsid=TRUE){
-  source = "DOD"
+toxval.load.dod <- function(toxval.db,source.db,log=F) {
+  printCurrentFunction(toxval.db)
+
+  #####################################################################
+  cat("start output log, log files for each source can be accessed from output_log folder\n")
+  #####################################################################
+  source <- "DOD"
   source_table = "source_dod_meg"
   verbose = log
   #####################################################################
@@ -14,7 +19,7 @@ toxval.load.dod <- function(toxvaldb,source.db, log=FALSE, remove_null_dtxsid=TR
   #####################################################################
   if(log) {
     con1 = file.path(toxval.config()$datapath,paste0(source,"_",Sys.Date(),".log"))
-    con1 = logr::log_open(con1)
+    con1 = log_open(con1)
     con = file(paste0(toxval.config()$datapath,source,"_",Sys.Date(),".log"))
     sink(con, append=TRUE)
     sink(con, append=TRUE, type="message")
@@ -23,7 +28,6 @@ toxval.load.dod <- function(toxvaldb,source.db, log=FALSE, remove_null_dtxsid=TR
   cat("clean source_info by source\n")
   #####################################################################
   import.source.info.by.source(toxval.db, source)
-
   #####################################################################
   cat("clean by source\n")
   #####################################################################
@@ -32,28 +36,42 @@ toxval.load.dod <- function(toxvaldb,source.db, log=FALSE, remove_null_dtxsid=TR
   #####################################################################
   cat("load data to res\n")
   #####################################################################
-  # Whether to remove records with NULL DTXSID values
-  if(!remove_null_dtxsid){
-    query = paste0("select * from ",source_table)
-  } else {
-    query = paste0("select * from ",source_table, " ",
-                   # Filter out records without curated chemical information
-                   "WHERE chemical_id IN (SELECT chemical_id FROM source_chemical WHERE dtxsid is NOT NULL)")
-  }
-  res = runQuery(query,source.db,TRUE,FALSE)
-
-  res = readxl::read_xlsx("import_dod_meg_source_output.xlsx")
-  res = res[,!names(res) %in% toxval.config()$non_hash_cols[!toxval.config()$non_hash_cols %in% c("chemical_id")]]
+  query = paste0("select * from ",source_table)
+  res = runQuery(query,source.db,T,F)
+  res = res[ , !(names(res) %in% c("source_id","clowder_id","parent_hash","create_time","modify_time","created_by"))]
+  res = res[ , !(names(res) %in% c("qc_flags","qc_notes","version","parent_chemical_id"))]
   res$source = source
   res$details_text = paste(source,"Details")
-  print(paste0("Dimensions of source data: ", toString(dim(res))))
+  print(dim(res))
 
   #####################################################################
-  cat("Add code to deal with specific issues for this source\n")
+  cat("Add the code from the original version from Aswani\n")
   #####################################################################
+  # query <- "select source_hash ,casrn, name, 'MEG' as toxval_type, concat(subtype, ' ', MEG_type, ' ', method) as toxval_subtype,
+  # toxval_numeric, units as toxval_units, 'DOD' as source, subsource, route as exposure_route, duration, document_name
+  # from dod_meg_2013"
+  #
+  # res <- runQuery(query,source.db,T,F)
+  res = separate(res, duration, c("study_duration_value", "study_duration_units"), sep = "[ -]")
+  res[res$study_duration_units=="h","study_duration_units"]="hour"
+  res <- res[!is.na(res[,"casrn"]),]
+  colnames(res)[which(names(res) == "units")] = "toxval_units"
 
-  # Source-specific transformations handled in import script.
-  # Other column assignments handled later in load script.
+  res$toxval_type = "MEG"
+  res$toxval_subtype = paste(res$subtype,res$meg_type,res$method)
+
+  res$source <- source
+  res$year <- "2013"
+  res$source_url <- "https://phc.amedd.army.mil/Pages/Library.aspx?queries[series]=PHC+Technical+Guide"
+  #res$human_eco <- "eco"
+  res <- unique(res)
+  if(is.element("species_original",names(res))) res[,"species_original"] <- tolower(res[,"species_original"])
+  res <- fill.toxval.defaults(toxval.db,res)
+  res <- generate.originals(toxval.db,res)
+  res[,"subsource"] <- "TG 230 Military Exposure Guidelines Table"
+  res[is.na(res[,"document_name"]),"document_name"] <- "-"
+  res <- unique(res)
+  colnames(res)[which(names(res) == "route")] = "exposure_route"
 
   #####################################################################
   cat("find columns in res that do not map to toxval or record_source\n")
@@ -62,18 +80,10 @@ toxval.load.dod <- function(toxvaldb,source.db, log=FALSE, remove_null_dtxsid=TR
   cols2 = runQuery("desc toxval",toxval.db)[,1]
   cols = unique(c(cols1,cols2))
   colnames(res)[which(names(res) == "species")] = "species_original"
-  res = res[ , !(names(res) %in% c("record_url","short_ref"))]
+  res = res[ , !(names(res) %in% c("record_url","short_ref","id","meg_type","t","route","method","subtype"))]
   nlist = names(res)
   nlist = nlist[!is.element(nlist,c("casrn","name"))]
   nlist = nlist[!is.element(nlist,cols)]
-
-  # Remove unnecessary columns ("columns to be dealt with)
-  res = res %>% dplyr::select(!nlist)
-
-  nlist = names(res)
-  nlist = nlist[!is.element(nlist,c("casrn","name"))]
-  nlist = nlist[!is.element(nlist,cols)]
-
   if(length(nlist)>0) {
     cat("columns to be dealt with\n")
     print(nlist)
@@ -81,31 +91,31 @@ toxval.load.dod <- function(toxvaldb,source.db, log=FALSE, remove_null_dtxsid=TR
   }
   print(dim(res))
 
+  # examples ...
+  # names(res)[names(res) == "source_url"] = "url"
+  # colnames(res)[which(names(res) == "phenotype")] = "critical_effect"
+
   #####################################################################
   cat("Generic steps \n")
   #####################################################################
-  res = distinct(res)
+  res = unique(res)
   res = fill.toxval.defaults(toxval.db,res)
   res = generate.originals(toxval.db,res)
+  if(is.element("species_original",names(res))) res[,"species_original"] = tolower(res[,"species_original"])
   res$toxval_numeric = as.numeric(res$toxval_numeric)
-  print(paste0("Dimensions of source data after originals added: ", toString(dim(res))))
+  print(dim(res))
   res=fix.non_ascii.v2(res,source)
-  # Remove excess whitespace
-  res = res %>%
-    dplyr::mutate(dplyr::across(where(is.character), stringr::str_squish))
-  res = distinct(res)
-  res = res[, !names(res) %in% c("casrn","name")]
-  print(paste0("Dimensions of source data after ascii fix and removing chemical info: ", toString(dim(res))))
+  res = data.frame(lapply(res, function(x) if(class(x)=="character") trimws(x) else(x)), stringsAsFactors=F, check.names=F)
+  res = unique(res)
+  res = res[,!is.element(names(res),c("casrn","name"))]
+  print(dim(res))
 
   #####################################################################
   cat("add toxval_id to res\n")
   #####################################################################
   count = runQuery("select count(*) from toxval",toxval.db)[1,1]
-  if(count==0) {
-    tid0 = 1
-  } else {
-    tid0 = runQuery("select max(toxval_id) from toxval",toxval.db)[1,1] + 1
-  }
+  if(count==0) tid0 = 1
+  else tid0 = runQuery("select max(toxval_id) from toxval",toxval.db)[1,1] + 1
   tids = seq(from=tid0,to=tid0+nrow(res)-1)
   res$toxval_id = tids
   print(dim(res))
@@ -126,52 +136,44 @@ toxval.load.dod <- function(toxvaldb,source.db, log=FALSE, remove_null_dtxsid=TR
   #####################################################################
   cat("add extra columns to refs\n")
   #####################################################################
-  refs$record_source_type = "government document"
-  refs$record_source_note = "This reference compiles DOD values across many chemicals"
-  refs$record_source_level = "primary (risk assessment values)"
-
-  # Add extra columns as specified in previous load script
+  refs$record_source_type <- "government document"
+  refs$record_source_note <- "This reference compiles DOD values across many chemicals"
+  refs$record_source_level <- "primary (risk assessment values)"
   refs$long_ref <- "U.S. Army Public Health Command (2013) TG 230 Military Exposure Guidelines Table. Army Public Health Center."
   refs$title <- "TG 230 Military Exposure Guidelines Table"
   refs$author <- "U.S. Army Public Health Command"
   refs$url <- "https://phc.amedd.army.mil/Pages/Library.aspx?queries[series]=PHC+Technical+Guide"
   refs$year <- "2013"
-  refs$document_name <- "TG230MilitaryExposureGuidelines.xls"
+  refs$document_name <- "TG230MilitaryExposureGuidelines.xlsx"
 
-  print(paste0("Dimensions of references after adding ref columns: ", toString(dim(refs))))
+  print(dim(res))
 
   #####################################################################
   cat("load res and refs to the database\n")
   #####################################################################
-  res = distinct(res)
-  refs = distinct(refs)
+  res = unique(res)
+  refs = unique(refs)
   res$datestamp = Sys.Date()
-  res$source_table = source_table
-  res$source_url = "https://phc.amedd.army.mil/Pages/Library.aspx?queries[series]=PHC+Technical+Guide"
-  res$subsource_url = "-"
-  res$year = "2013"
-  res$details_text = paste(source,"Details")
   #for(i in 1:nrow(res)) res[i,"toxval_uuid"] = UUIDgenerate()
   #for(i in 1:nrow(refs)) refs[i,"record_source_uuid"] = UUIDgenerate()
   runInsertTable(res, "toxval", toxval.db, verbose)
-  print(paste0("Dimensions of source data pushed to toxval: ", toString(dim(res))))
   runInsertTable(refs, "record_source", toxval.db, verbose)
-  print(paste0("Dimensions of references pushed to record_source: ", toString(dim(refs))))
+  print(dim(res))
 
   #####################################################################
   cat("do the post processing\n")
   #####################################################################
-  toxval.load.postprocess(toxval.db,source.db,source,do.convert.units=FALSE, remove_null_dtxsid=remove_null_dtxsid)
+  toxval.load.postprocess(toxval.db,source.db,source)
 
   if(log) {
     #####################################################################
     cat("stop output log \n")
     #####################################################################
     closeAllConnections()
-    logr::log_close()
-    output_message = read.delim(paste0(toxval.config()$datapath,source,"_",Sys.Date(),".log"), stringsAsFactors = FALSE, header = FALSE)
+    log_close()
+    output_message = read.delim(paste0(toxval.config()$datapath,source,"_",Sys.Date(),".log"), stringsAsFactors = F, header = F)
     names(output_message) = "message"
-    output_log = read.delim(paste0(toxval.config()$datapath,"log/",source,"_",Sys.Date(),".log"), stringsAsFactors = FALSE, header = FALSE)
+    output_log = read.delim(paste0(toxval.config()$datapath,"log/",source,"_",Sys.Date(),".log"), stringsAsFactors = F, header = F)
     names(output_log) = "log"
     new_log = log_message(output_log, output_message[,1])
     writeLines(new_log, paste0(toxval.config()$datapath,"output_log/",source,"_",Sys.Date(),".txt"))
@@ -179,13 +181,4 @@ toxval.load.dod <- function(toxvaldb,source.db, log=FALSE, remove_null_dtxsid=TR
   #####################################################################
   cat("finish\n")
   #####################################################################
-  return(0)
-
-  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 }
