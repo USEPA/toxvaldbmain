1fad7f33487c30a7ed20e9da4e2e0784dce5c0d3
Groover <Groover.Maxwell@epa.gov>
2023-12-15 16:17:56 -0500

    Removed transformation logic that was transported to import script. Waiting for new import to occur before testing if load script works as is.

--- R/toxval.load.hawc.R
+++ R/toxval.load.hawc.R
@@ -10,7 +10,7 @@ toxval.load.hawc <- function(toxval.db, source.db, log=FALSE, remove_null_dtxsid
   source <- "HAWC Project"
   source_table = "source_hawc"
   verbose = log
-  runQuery("update source_chemical set source='HAWC Project' where source='HAWC'",source.db)
+  #runQuery("update source_chemical set source='HAWC Project' where source='HAWC'",source.db)
   #####################################################################
   cat("start output log, log files for each source can be accessed from output_log folder\n")
   #####################################################################
@@ -48,93 +48,9 @@ toxval.load.hawc <- function(toxval.db, source.db, log=FALSE, remove_null_dtxsid
   print(paste0("Dimensions of source data: ", toString(dim(res))))
 
   #####################################################################
-  cat("Add the code from the original version from Aswani\n")
+  cat("Add code to deal with specific issues for this source\n")
   #####################################################################
-  # Old code, unnecessary
-  # unique(res$study_type)
-  # res$study_type <- tolower(res$study_type)
-  # para_vals <- grep("\\(",res$study_type)
-  # res[para_vals, "study_type"] <- gsub("(.*)(\\s+\\(.*)","\\1",res[para_vals, "study_type"])
-  ##### fix exposure_route
-  unique(res$exposure_route)
-  res$exposure_route <- tolower(res$exposure_route)
-  oral_vals <- grep("oral", res$exposure_route)
-  res[oral_vals, "exposure_route"] <- gsub("(oral)(\\s+.*)","\\1",res[oral_vals, "exposure_route"])
-  injection_vals <- grep("injection", res$exposure_route)
-  res[injection_vals, "exposure_route"] <- gsub("(.*)(\\s+injection)","\\1",res[injection_vals, "exposure_route"])
-  ####### fix exposure_method
-  unique(res$exposure_method)
-  res$exposure_method <- tolower(res$exposure_method)
-  oral_vals <- grep("oral", res$exposure_method)
-  res[oral_vals, "exposure_method"] <- gsub("(oral\\s+)(.*)","\\2",res[oral_vals, "exposure_method"])
-  injection_vals <- grep("injection", res$exposure_method)
-  res[injection_vals, "exposure_method"] <- gsub("(.*\\s+)(injection)","\\2",res[injection_vals, "exposure_method"])
-  res$exposure_method <- tolower(res$exposure_method)
-
-  ######### fix study duration value and units
-  # #hour vals
-  hour_vals <- grep("hour", res$study_duration_value, ignore.case = T)
-  res[hour_vals,"study_duration_units"] <- "hour"
-  res[hour_vals,"study_duration_value"] <- gsub("^([0-9]+)(\\s+)(hours)","\\1",res[hour_vals,"study_duration_value"])
-  # day vals
-  day_vals <- grep("day", res$study_duration_value, ignore.case = T)
-  res[day_vals,"study_duration_units"] <- "day"
-  res[day_vals,"study_duration_value"] <- gsub("^([0-9]+)(\\s+)(days)(.*)","\\1",res[day_vals,"study_duration_value"])
-  day_vals <- grep("day", res$study_duration_value, ignore.case = T)
-  res[day_vals,"study_duration_value"] <- gsub("^([0-9]+\\-)([0-9]+)(\\s+)(days)(.*)","\\2",res[day_vals,"study_duration_value"])
-  #week vals
-  week_vals <- grep("week", res$study_duration_value, ignore.case = T)
-  res[week_vals,"study_duration_units"] <- "week"
-  res[week_vals,"study_duration_value"] <- gsub("^([0-9]+)(\\s+)(weeks)(.*)","\\1",res[week_vals,"study_duration_value"])
-  week_vals <- grep("week", res$study_duration_value, ignore.case = T)
-  res[week_vals,"study_duration_value"] <- gsub("^(.*[^0-9]+)([0-9]+)(\\s+)(weeks)(.*)","\\2",res[week_vals,"study_duration_value"])
-  #month vals (without PND)
-  month_vals <- grep("months$", res$study_duration_value, ignore.case = T)
-  res[month_vals,"study_duration_units"] <- "month"
-  res[month_vals,"study_duration_value"] <- gsub("^([0-9]+)(\\s+)(months)","\\1",res[month_vals,"study_duration_value"])
-  #one time vals
-  one_time_vals <- grep("one time", res$study_duration_value, ignore.case = T)
-  res[one_time_vals,"study_duration_units"] <- "one time"
-  res[one_time_vals,"study_duration_value"] <- "1"
-  # GD range vals
-  GD_vals <- grep("GD\\s+.*\\-[^a-zA-Z]+$", res$study_duration_value, ignore.case = T)
-  res[GD_vals,"study_duration_units"] <- "GD"
-  res[GD_vals,"study_duration_value"] <- gsub("^(GD)(\\s+.*\\-\\s*)(.*)","\\3",res[GD_vals,"study_duration_value"])
-
-  # GD until vals
-  GD_until_vals <- grep("GD.*until.*[^0]$", res$study_duration_value, ignore.case = T)
-  res[GD_until_vals,"study_duration_units"] <- "GD"
-  res[GD_until_vals,"study_duration_value"] <- gsub("^(GD.*GD\\s+)(.*)","\\2",res[GD_until_vals,"study_duration_value"])
-
-  GD_until_zero_vals <- grep("GD.*until.*[0]$", res$study_duration_value, ignore.case = T)
-  res[GD_until_zero_vals,"study_duration_units"] <- res[GD_until_zero_vals,"study_duration_value"]
-  res[GD_until_zero_vals,"study_duration_value"] <- ""
-
-  #PND range vals
-  PND_vals <- grep(".*PND\\s*.*[^0a-zA-Z]$", res$study_duration_value, ignore.case = T)
-  res[PND_vals,"study_duration_units"] <- "PND"
-  res[PND_vals,"study_duration_value"] <- gsub("^(.*PND\\s*)(\\d+)","\\2",res[PND_vals,"study_duration_value"])
-  res[which(res$study_duration_value == "2-15" & res$study_duration_units == "PND"),"study_duration_value"] <- gsub("(\\d+\\-)(\\d+)","\\2",res[which(res$study_duration_value == "2-15" & res$study_duration_units == "PND"),"study_duration_value"])
-  PND_vals <- grep(".*PND\\s*[^0]\\d+$", res$study_duration_value, ignore.case = T)
-  res[PND_vals,"study_duration_units"] <- "PND"
-  res[PND_vals,"study_duration_value"] <- gsub("^(.*PND\\s*)(\\d+)(.*?)","\\2",res[PND_vals,"study_duration_value"])
-  res[which(res$study_duration_value == "21, not PND 0" & res$study_duration_units == "PND"),"study_duration_value"] <- gsub("(\\d+)(\\,.*)","\\1",res[which(res$study_duration_value == "21, not PND 0" & res$study_duration_units == "PND"),"study_duration_value"])
-  # GD or PND zero vals
-  zero_vals <- grep("PND0|GD0", res$study_duration_value, ignore.case = T)
-  res[zero_vals,"study_duration_units"] <- res[zero_vals,"study_duration_value"]
-  res[zero_vals,"study_duration_value"] <- ""
-  # 1 OR 2 years vals
-  or_vals <- grep("or", res$study_duration_value, ignore.case = T)
-  res[or_vals,"study_duration_units"] <- gsub("(.*or\\s+)(\\d+)(\\s+)(\\w+)","\\4",res[or_vals,"study_duration_value"])
-  res[or_vals,"study_duration_value"] <- gsub("(.*or\\s+)(\\d+)(\\s+)(\\w+)","\\2",res[or_vals,"study_duration_value"])
-  # PND 3-10 vals
-  PND_vals <- grep("PND", res$study_duration_value, ignore.case = T)
-  res[PND_vals,"study_duration_units"] <-"PND"
-  res[PND_vals,"study_duration_value"] <- gsub("(PND.*\\-)(\\d+)","\\2",res[PND_vals,"study_duration_value"])
-  res[which(res$study_duration_value == "-"),"study_duration_value"] <- ""
-
-  #res$study_duration_value <- as.numeric(res$study_duration_value)
-
+  browser()
   cremove = c("assessment","target","noel_original","loel_original","fel_original",
             "endpoint_url_original","study_id","authors_short","full_text_url","study_url_original",
             "experiment_name","experiment_type","chemical_source","guideline_compliance","dosing_regime_id",
