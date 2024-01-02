c9c6324fa1aa0f98c5d557df2f154f31866d23a9
Winter <Winter.Samuel@epa.gov>
2024-01-02 17:52:21 -0500

    Updated EnviroTox load script to account for import script updates

--- R/deprecated/load scripts/toxval.load.envirotox_20210928_v94.R
+++ R/deprecated/load scripts/toxval.load.envirotox_20210928_v94.R
@@ -1,13 +1,14 @@
-#--------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------
 #' Load EnviroTox data from toxval_source to toxval
 #'
-#' @param toxval.db The database version to use
-#' @param source.db The source database
+#' @param toxval.db The version of toxval into which the tables are loaded.
+#' @param source.db The source database to use.
 #' @param log If TRUE, send output to a log file
-#' @param remove_null_dtxsid If TRUE, delete source records without curated DTXSID value
+#' @export
 #--------------------------------------------------------------------------------------
-toxval.load.envirotox <- function(toxval.db,source.db, log=FALSE, remove_null_dtxsid=TRUE){
-  source = "EnviroTox_v2"
+toxval.load.envirotox <- function(toxval.db, source.db, log=F) {
+  printCurrentFunction(toxval.db)
+  source <- "EnviroTox_v2"
   source_table = "source_envirotox"
   verbose = log
   #####################################################################
@@ -15,7 +16,7 @@ toxval.load.envirotox <- function(toxval.db,source.db, log=FALSE, remove_null_dt
   #####################################################################
   if(log) {
     con1 = file.path(toxval.config()$datapath,paste0(source,"_",Sys.Date(),".log"))
-    con1 = logr::log_open(con1)
+    con1 = log_open(con1)
     con = file(paste0(toxval.config()$datapath,source,"_",Sys.Date(),".log"))
     sink(con, append=TRUE)
     sink(con, append=TRUE, type="message")
@@ -24,7 +25,6 @@ toxval.load.envirotox <- function(toxval.db,source.db, log=FALSE, remove_null_dt
   cat("clean source_info by source\n")
   #####################################################################
   import.source.info.by.source(toxval.db, source)
-
   #####################################################################
   cat("clean by source\n")
   #####################################################################
@@ -33,25 +33,40 @@ toxval.load.envirotox <- function(toxval.db,source.db, log=FALSE, remove_null_dt
   #####################################################################
   cat("load data to res\n")
   #####################################################################
-  # Whether to remove records with NULL DTXSID values
-  if(!remove_null_dtxsid){
-    query = paste0("select * from ",source_table)
-  } else {
-    query = paste0("select * from ",source_table, " ",
-                   # Filter out records without curated chemical information
-                   "WHERE chemical_id IN (SELECT chemical_id FROM source_chemical WHERE dtxsid is NOT NULL)")
-  }
-  res = runQuery(query,source.db,TRUE,FALSE)
-  res = res[,!names(res) %in% toxval.config()$non_hash_cols[!toxval.config()$non_hash_cols %in%
-                                                              c("chemical_id", "document_name", "source_hash", "qc_status")]]
+  query = paste0("select * from ",source_table)
+  res = runQuery(query,source.db,T,F)
+  res = res[ , !(names(res) %in% c("source_id","clowder_id","parent_hash","create_time","modify_time","created_by"))]
+  res = res[ , !(names(res) %in% c("qc_flags","qc_notes","version","parent_chemical_id"))]
   res$source = source
   res$details_text = paste(source,"Details")
-  print(paste0("Dimensions of source data: ", toString(dim(res))))
+  print(dim(res))
+  #res = res[1:1000,]
 
   #####################################################################
-  cat("Add code to deal with specific issues for this source\n")
+  cat("Add the code from the original version from Aswani\n")
   #####################################################################
-  # Source-specific transformations handled in import script
+  #names(res)[is.element(names(res),"source")] <- "long_ref"
+  names(res)[is.element(names(res),"latin_name")] <- "species_original"
+  names(res)[is.element(names(res),"effect")] <- "critical_effect"
+  names(res)[is.element(names(res),"effect_value")] <- "toxval_numeric"
+  names(res)[is.element(names(res),"unit")] <- "toxval_units"
+  names(res)[is.element(names(res),"test_type")] <- "study_type"
+  names(res)[is.element(names(res),"test_statistic")] <- "toxval_type"
+
+  # some of the duration_(days) field have only units represented in them eg:Day(s) post-hatch,
+  # Brood or litter etc in duration_(days). hence extracting their respective values from duration field
+  res$study_duration_value <- res$duration_days
+  res$study_duration_units <- "day"
+  res[grep("[a-zA-Z]+",res$duration_days),"study_duration_value"] <- gsub("(^\\d+)(\\s+)(.*)","\\1",res[grep("[a-zA-Z]+",res$duration_days),"duration"])
+  res[grep("[a-zA-Z]+",res$duration_days),"study_duration_units"] <- gsub("(^\\d+)(\\s+)(.*)","\\3", res[grep("[a-zA-Z]+",res$duration_days),"duration"])
+  res$toxval_numeric <- as.numeric(res$toxval_numeric)
+  res[which(res$study_duration_value == "-"),"study_duration_value"] <- ""
+
+  res$study_duration_value <- as.numeric(res$study_duration_value)
+  res[grep("A",res$study_type),"study_type"] <- "acute"
+  res[grep("C",res$study_type),"study_type"] <- "chronic"
+  res = res[ , !(names(res) %in% c("reported_chemical_name","original_cas","trophic_level","duration",
+                                   "duration_days","version","effect_is_5x_above_water_solubility","duration_hours"))]
 
   #####################################################################
   cat("find columns in res that do not map to toxval or record_source\n")
@@ -59,54 +74,44 @@ toxval.load.envirotox <- function(toxval.db,source.db, log=FALSE, remove_null_dt
   cols1 = runQuery("desc record_source",toxval.db)[,1]
   cols2 = runQuery("desc toxval",toxval.db)[,1]
   cols = unique(c(cols1,cols2))
+  colnames(res)[which(names(res) == "species")] = "species_original"
   res = res[ , !(names(res) %in% c("record_url","short_ref"))]
   nlist = names(res)
   nlist = nlist[!is.element(nlist,c("casrn","name"))]
   nlist = nlist[!is.element(nlist,cols)]
-
-  # Remove columns that are not used in toxval
-  res = res %>% dplyr::select(!dplyr::any_of(nlist))
-
-  nlist = names(res)
-  nlist = nlist[!is.element(nlist,c("casrn","name"))]
-  nlist = nlist[!is.element(nlist,cols)]
-
   if(length(nlist)>0) {
     cat("columns to be dealt with\n")
     print(nlist)
     browser()
   }
   print(dim(res))
+  # examples ...
+  # names(res)[names(res) == "source_url"] = "url"
+  # colnames(res)[which(names(res) == "phenotype")] = "critical_effect"
 
   #####################################################################
   cat("Generic steps \n")
   #####################################################################
-  res = distinct(res)
+  res = unique(res)
   res = fill.toxval.defaults(toxval.db,res)
   res = generate.originals(toxval.db,res)
+  if(is.element("species_original",names(res))) res[,"species_original"] = tolower(res[,"species_original"])
   res$toxval_numeric = as.numeric(res$toxval_numeric)
-  print(paste0("Dimensions of source data after originals added: ", toString(dim(res))))
+  print(dim(res))
   res=fix.non_ascii.v2(res,source)
-  # Remove excess whitespace
-  res = res %>%
-    dplyr::mutate(dplyr::across(where(is.character), stringr::str_squish))
-  res = distinct(res)
-  res = res[, !names(res) %in% c("casrn","name")]
-  print(paste0("Dimensions of source data after ascii fix and removing chemical info: ", toString(dim(res))))
-
+  res = data.frame(lapply(res, function(x) if(class(x)=="character") trimws(x) else(x)), stringsAsFactors=F, check.names=F)
+  res = unique(res)
+  res = res[,!is.element(names(res),c("casrn","name"))]
+  print(dim(res))
   #####################################################################
   cat("add toxval_id to res\n")
   #####################################################################
   count = runQuery("select count(*) from toxval",toxval.db)[1,1]
-  if(count==0) {
-    tid0 = 1
-  } else {
-    tid0 = runQuery("select max(toxval_id) from toxval",toxval.db)[1,1] + 1
-  }
+  if(count==0) tid0 = 1
+  else tid0 = runQuery("select max(toxval_id) from toxval",toxval.db)[1,1] + 1
   tids = seq(from=tid0,to=tid0+nrow(res)-1)
   res$toxval_id = tids
   print(dim(res))
-
   #####################################################################
   cat("pull out record source to refs\n")
   #####################################################################
@@ -126,38 +131,38 @@ toxval.load.envirotox <- function(toxval.db,source.db, log=FALSE, remove_null_dt
   refs$record_source_type = "website"
   refs$record_source_note = "to be cleaned up"
   refs$record_source_level = "primary (risk assessment values)"
-  print(paste0("Dimensions of references after adding ref columns: ", toString(dim(refs))))
+  print(dim(res))
 
   #####################################################################
   cat("load res and refs to the database\n")
   #####################################################################
-  res = distinct(res)
-  refs = distinct(refs)
+  res = unique(res)
+  refs = unique(refs)
   res$datestamp = Sys.Date()
   res$source_table = source_table
+  res$source_url = "https://envirotoxdatabase.org/"
   res$subsource_url = "-"
   res$details_text = paste(source,"Details")
   #for(i in 1:nrow(res)) res[i,"toxval_uuid"] = UUIDgenerate()
   #for(i in 1:nrow(refs)) refs[i,"record_source_uuid"] = UUIDgenerate()
   runInsertTable(res, "toxval", toxval.db, verbose)
-  print(paste0("Dimensions of source data pushed to toxval: ", toString(dim(res))))
   runInsertTable(refs, "record_source", toxval.db, verbose)
-  print(paste0("Dimensions of references pushed to record_source: ", toString(dim(refs))))
+  print(dim(res))
 
   #####################################################################
   cat("do the post processing\n")
   #####################################################################
-  toxval.load.postprocess(toxval.db,source.db,source,do.convert.units=FALSE, remove_null_dtxsid=remove_null_dtxsid)
+  toxval.load.postprocess(toxval.db,source.db,source)
 
   if(log) {
     #####################################################################
     cat("stop output log \n")
     #####################################################################
     closeAllConnections()
-    logr::log_close()
-    output_message = read.delim(paste0(toxval.config()$datapath,source,"_",Sys.Date(),".log"), stringsAsFactors = FALSE, header = FALSE)
+    log_close()
+    output_message = read.delim(paste0(toxval.config()$datapath,source,"_",Sys.Date(),".log"), stringsAsFactors = F, header = F)
     names(output_message) = "message"
-    output_log = read.delim(paste0(toxval.config()$datapath,"log/",source,"_",Sys.Date(),".log"), stringsAsFactors = FALSE, header = FALSE)
+    output_log = read.delim(paste0(toxval.config()$datapath,"log/",source,"_",Sys.Date(),".log"), stringsAsFactors = F, header = F)
     names(output_log) = "log"
     new_log = log_message(output_log, output_message[,1])
     writeLines(new_log, paste0(toxval.config()$datapath,"output_log/",source,"_",Sys.Date(),".txt"))
@@ -165,13 +170,4 @@ toxval.load.envirotox <- function(toxval.db,source.db, log=FALSE, remove_null_dt
   #####################################################################
   cat("finish\n")
   #####################################################################
-  return(0)
-
-  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 }
