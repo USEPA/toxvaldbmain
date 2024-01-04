1e71ecf31a3ba4e3f17ff744c869c99df04ec077
Taylor Wall <wall.jonathan@epa.gov>
2024-01-04 14:39:13 -0500

    Removed non-toxval field.

--- R/toxval.load.hawc.R
+++ R/toxval.load.hawc.R
@@ -55,7 +55,7 @@ toxval.load.hawc <- function(toxval.db, source.db, log=FALSE, remove_null_dtxsid
             "endpoint_url_original","study_id","authors_short","full_text_url","study_url_original",
             "experiment_name","experiment_type","chemical_source","guideline_compliance","dosing_regime_id",
             "route_of_exposure","exposure_duration_value","exposure_duration_text","doses","endpoint_url",
-            "study_url")
+            "study_url", "study_duration_qualifier")
   res = res[ , !(names(res) %in% cremove)]
 
   #####################################################################
