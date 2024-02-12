c12e6a5f440b980428cdd6ba8105a795907b8289
Taylor Wall <wall.jonathan@epa.gov>
2024-02-12 10:32:09 -0500

    Updated to use lapply instead of for-loops.

--- R/set_extraction_doc_clowder_id.R
+++ R/set_extraction_doc_clowder_id.R
@@ -12,16 +12,18 @@ set_extraction_doc_clowder_id <- function(toxval.db, source.db, source=NULL){
   } else {
     slist = runQuery("select distinct source from toxval",toxval.db)[,1]
   }
-  res <- data.frame()
-  for(source in slist) {
-    message("Pulling source ", which(source == slist), " of ", length(slist))
+
+  # Get a list of dataframes by source, then combine
+  res = lapply(slist, function(source){
+    message("Pulling source ", source, " (", which(source == slist), " of ", length(slist), ")")
     # Get source table based on source name from toxval table
     src_tbl = runQuery(paste0("SELECT distinct source_table FROM ", toxval.db, ".toxval WHERE source = '", source, "'"),
-                       toxval.db)
-    src_tables = unique(src_tbl$source_table)
+                       toxval.db) %>%
+      dplyr::pull(source_table)
+
     # Loop over all source_tables per source (for IUCLID's multiple source tables)
-    for(src in src_tables){
-      print(source)
+    lapply(src_tbl, function(src){
+      cat(paste0("...", src, "\n"))
       res0 = runQuery(paste0("select t.toxval_id, d.clowder_id, t.source, d.document_type, d.url, d.document_name,
                            d.title, d.author, d.year, d.doi, d.clowder_metadata ",
                              "FROM ", toxval.db, ".toxval t ",
@@ -29,12 +31,15 @@ set_extraction_doc_clowder_id <- function(toxval.db, source.db, source=NULL){
                              "LEFT JOIN ", source.db, ".documents d ON dr.fk_doc_id = d.id ",
                              "WHERE dr.source_table ='", src, "'"),
                       toxval.db)
-      # Combine results
-      res <- bind_rows(res, res0)
-    }
-  }
-  res <- res %>% dplyr::rename(clowder_doc_id = clowder_id,
-                               record_source_level = document_type)
+    }) %>%
+      dplyr::bind_rows() %>%
+      return()
+  }) %>%
+    dplyr::bind_rows() %>%
+    dplyr::rename(clowder_doc_id = clowder_id,
+                  clowder_doc_metadata = clowder_metadata,
+                  record_source_level = document_type)
+
   message("Pushing clowder_doc_id information to record_source...")
   # Run insert query
   runInsertTable(mat=res, table='record_source', db=toxval.db)
