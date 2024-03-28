fc9cdd9b07efe90c87e3daa20be93ce0f1640029
Taylor Wall <wall.jonathan@epa.gov>
2024-03-28 16:05:35 -0400

    HotFix: Loads without source_hash fixed.

--- R/toxval.load.epa_ow_npdwr.R
+++ R/toxval.load.epa_ow_npdwr.R
@@ -42,9 +42,8 @@ toxval.load.epa_ow_npdwr <- function(toxval.db,source.db, log=FALSE, remove_null
                    "WHERE chemical_id IN (SELECT chemical_id FROM source_chemical WHERE dtxsid is NOT NULL)")
   }
   res = runQuery(query,source.db,TRUE,FALSE)
-  res = res[,!names(res) %in%
-              toxval.config()$non_hash_cols[!toxval.config()$non_hash_cols %in%
-                                              c("chemical_id")]]
+  res = res[,!names(res) %in% toxval.config()$non_hash_cols[!toxval.config()$non_hash_cols %in%
+                                                              c("chemical_id", "document_name", "source_hash", "qc_status")]]
   res$source = source
   res$details_text = paste(source,"Details")
   print(paste0("Dimensions of source data: ", toString(dim(res))))
