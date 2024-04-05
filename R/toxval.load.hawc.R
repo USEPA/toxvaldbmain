5d500ef76ae17739a1f2dac3e1970407e6d50386
Taylor Wall <wall.jonathan@epa.gov>
2024-04-05 14:13:44 -0400

    Update to check if study_duration contains GD AND PND to set to NA.

--- R/toxval.load.hawc.R
+++ R/toxval.load.hawc.R
@@ -53,16 +53,16 @@ toxval.load.hawc <- function(toxval.db, source.db, log=FALSE, remove_null_dtxsid
   #####################################################################
 
   res = res %>% dplyr::mutate(
-    # Set NA study_duration for GD+PND values
-    study_duration_units = dplyr::case_when(
-      grepl("GD|PND", study_duration_value) ~ as.character(NA),
-      TRUE ~ study_duration_units
-    ),
+    # Set NA study_duration for entries with both GD and PND values
     study_duration_value = dplyr::case_when(
-      grepl("GD|PND", study_duration_value) ~ as.character(NA),
+      grepl("PND", study_duration_value) & grepl("GD", study_duration_value) ~ as.character(NA),
       TRUE ~ study_duration_value
     ),
-
+    # Set NA for units without values
+    study_duration_units = dplyr::case_when(
+      is.na(study_duration_value) ~ as.character(NA),
+      TRUE ~ study_duration_units
+    ),
     # Select higher value in ranged study_duration
     study_duration_value = study_duration_value %>%
       gsub(".+\\-", "", .) %>%
