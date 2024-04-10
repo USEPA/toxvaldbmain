b31545936c4229a2ac86ee38d9b080c890651dae
Winter <Winter.Samuel@epa.gov>
2024-04-10 11:16:17 -0400

    Handled case of GD,weeks study_duration

--- R/toxval.load.hawc.R
+++ R/toxval.load.hawc.R
@@ -56,6 +56,7 @@ toxval.load.hawc <- function(toxval.db, source.db, log=FALSE, remove_null_dtxsid
     # Set NA study_duration for entries with both GD and PND values
     study_duration_value = dplyr::case_when(
       grepl("PND", study_duration_value) & grepl("GD", study_duration_value) ~ as.character(NA),
+      grepl("weeks", study_duration_value) & grepl("GD", study_duration_value) ~ as.character(NA),
       TRUE ~ study_duration_value
     ),
     # Set NA for units without values
