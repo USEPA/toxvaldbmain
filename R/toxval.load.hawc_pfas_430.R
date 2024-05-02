b144677ed51b410d89c0165e63c286fd32e4f0b2
Winter <Winter.Samuel@epa.gov>
2024-05-02 11:35:38 -0400

    Replace NA study_duration values with '-'

--- R/toxval.load.hawc_pfas_430.R
+++ R/toxval.load.hawc_pfas_430.R
@@ -66,7 +66,7 @@ toxval.load.hawc_pfas_430 <- function(toxval.db, source.db, log=FALSE, remove_nu
     # Select higher value in ranged study_duration
     study_duration_value = study_duration_value %>%
       gsub(".+\\-", "", .) %>%
-      dplyr::na_if("-"),
+      dplyr::replace_na("-"),
     study_duration_units = study_duration_units %>%
       tidyr::replace_na("-")
   )
