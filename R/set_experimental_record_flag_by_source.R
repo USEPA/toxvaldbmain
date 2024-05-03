86232c730a1b01f36ac294c48a6755e4ab5d6997
Groover <Groover.Maxwell@epa.gov>
2024-05-03 12:12:46 -0400

    Updated to only update records that don't already have an experimental_record since it seems like we implemented in the import stage for a lot of sources. Also hardcoded the Copper Manufacturers value

--- R/set_experimental_record_flag_by_source.R
+++ R/set_experimental_record_flag_by_source.R
@@ -12,21 +12,33 @@ set_experimental_record_flag_by_source <- function(toxval.db, source=NULL){
   } else {
     slist = runQuery("select distinct source from toxval",toxval.db)[,1]
   }
+  subset_data <- runQuery(paste0("SELECT source, toxval_type, species_original, source_url, experimental_record
+                                 FROM toxval
+                                 WHERE source IN ('", paste(slist, collapse="','"), "')
+                                 AND (experimental_record = '-' OR experimental_record IS NULL)"),toxval.db)
+
   file = paste0(toxval.config()$datapath,"dictionary/toxval_source_type_species_20240225.xlsx")
   exp_rec_tag = read.xlsx(file)
 
-  res <- lapply(slist, function(source) {
-    dplyr::filter(exp_rec_tag, source == source)
-  })
-  res <- res[[1]]
-  res <- subset(res, select = -c(`X1`, `cancer`, `dev/repro`, `X9`))
+  #res <- lapply(slist, function(source) {
+  #  dplyr::filter(exp_rec_tag, source == source)
+  #})
+  #res <- res[[1]]
+  exp_rec_tag <- subset(exp_rec_tag, select = -c(`X1`, `cancer`, `dev/repro`, `X9`))
+  res <- merge(subset_data, exp_rec_tag, by=c("source", "toxval_type", "species_original", "source_url"), all.x = TRUE)
+
+  res$experimental_record <- res$experimental
+
+  if("Copper Manufacturers" %in% slist){
+    res$experimental_record[res$source == "Copper Manufacturers"] <- "Yes"
+  }
 
   if(nrow(res)){
     # Query to inner join and update toxval with experimental_record tag (temp table added/dropped)
     updateQuery = paste0("UPDATE toxval a INNER JOIN z_updated_df b
                         ON (a.source = b.source AND a.toxval_type = b.toxval_type
                         AND a.species_original = b.species_original AND a.source_url = b.source_url)
-                       SET a.experimental_record = b.experiment")
+                       SET a.experimental_record = b.experimental_record")
     # Run update query
     runUpdate(table="toxval", updateQuery=updateQuery, updated_df=res, db=toxval.db)
   }
