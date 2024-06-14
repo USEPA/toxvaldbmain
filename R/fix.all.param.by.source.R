e9961e55624bdc29019162e50f33f77256c2ca73
Taylor Wall <wall.jonathan@epa.gov>
2024-06-14 11:25:33 -0400

    Special query set exposure_route to oral.

--- R/fix.all.param.by.source.R
+++ R/fix.all.param.by.source.R
@@ -115,6 +115,13 @@ fix.all.param.by.source <- function(toxval.db, source=NULL,subsource=NULL, fill.
     where toxval_type in ('RFDo', 'Oral Slope Factor', 'oral TDI', 'oral SF', 'oral ADI', 'LDD50 (Lethal Dietary Dose)') and source = '",source,"'",query_addition)
     runInsert(query,toxval.db,T,F,T)
 
+    query = paste0("update toxval ",
+                   "SET exposure_route = 'oral' ",
+                   "WHERE (exposure_route = '-' or exposure_route_original = '-') and toxval_units = 'mg/kg-day' and ",
+                   "(toxval_type in ('NEL', 'LEL', 'LOEL', 'NOEL', 'NOAEL', 'LOAEL') or toxval_type like 'BMD%') and ",
+                   "source = '",source,"'",query_addition)
+    runQuery(query, toxval.db)
+
     cat("  study_duration_class\n")
     query = paste0("update toxval
     set study_duration_class = 'acute'
