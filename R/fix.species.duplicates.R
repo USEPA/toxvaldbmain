cf73368c057fb897b9d558604e3cede39a491ec0
Winter <Winter.Samuel@epa.gov>
2024-07-01 13:13:52 -0400

    Changed to work by source and check rather than update values

--- R/fix.species.duplicates.R
+++ R/fix.species.duplicates.R
@@ -1,33 +1,50 @@
 #--------------------------------------------------------------------------------------
 #'
-#' Fix species duplicates - same common name but multiple species_ids
+#' Check for species duplicates - same common name but multiple species_ids
 #'
 #' @param toxval.db The version of the database to use
+#' @param source The source to be checked. If source=NULL, check all sources
+#' @param subsource The subsource to be checked (NULL default)
 #' @export
 #--------------------------------------------------------------------------------------
-fix.species.duplicates <- function(toxval.db) {
+fix.species.duplicates <- function(toxval.db, source=NULL, subsource=NULL) {
   printCurrentFunction()
-  cat("Check for duplicates with same species_original but different species_ids\n")
-  query = paste0("select a.species_original, b.species_id,b.common_name from
-                   toxval a, species b where
-                   a.species_id=b.species_id
-                   and a.human_eco='human health'")
-  mat = unique(runQuery(query,toxval.db))
-  mat = mat[order(mat$species_original),]
-  x = mat$species_original
-  dups = x[duplicated(x)]
-  cat("Number of duplicates:",length(dups),"\n")
-  for(i in seq_len(length(dups))) {
-    spo = dups[i]
-    y = mat[is.element(mat$species_original,spo),]
-    y = y[order(y$species_id),]
-    if(is.element("Not Specified",y$common_name) || is.element("Not specified",y$common_name)) {
-      y = y[!is.element(y$common_name,"Not Specified"),]
-      y = y[!is.element(y$common_name,"Not specified"),]
-    }
-    cat(spo,":",nrow(y),"\n")
-    query = paste0("update toxval set species_id=",y[1,"species_id"]," where species_original='",spo,"'")
-    print(query)
-    runQuery(query,toxval.db)
+
+  slist = runQuery("select distinct source from toxval",toxval.db)[,1]
+  output_file = "Repo/species/duplicate_species_id_ALL SOURCES.xlsx"
+  if(!is.null(source)) {
+    slist = source
+    output_file = paste0("Repo/species/duplicate_species_id", source, ".xlsx")
+  }
+
+  slist = slist %>%
+    paste0(., collapse="', '")
+
+  # Handle addition of subsource for queries
+  query_addition = ""
+  if(!is.null(subsource)) {
+    query_addition = paste0(" AND a.subsource='", subsource, "'")
+  }
+
+  query = paste0("SELECT DISTINCT a.species_original, b.species_id, b.common_name ",
+                 "FROM toxval a LEFT JOIN species b ON a.species_id=b.species_id ",
+                 "WHERE a.human_eco='human health' AND a.source IN ('", slist, "')",
+                 query_addition)
+  mat = runQuery(query,toxval.db)
+
+  duplicate_species = mat %>%
+    dplyr::distinct() %>%
+    dplyr::group_by(species_original) %>%
+    dplyr::mutate(n = dplyr::n()) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(n > 1) %>%
+    dplyr::select(-n)
+
+  cat("Number of species duplicates:",nrow(duplicate_species),"\n")
+
+  if(nrow(duplicate_species)) {
+    cat("Duplicate species identified and written to", output_file, "\n")
+    writexl::write_xlsx(duplicate_species, output_file)
+    browser()
   }
 }
