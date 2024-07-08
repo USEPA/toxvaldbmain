a14a99dcaf07722c7bdef898e20fcc6c32a003f4
Winter <Winter.Samuel@epa.gov>
2024-07-08 17:46:49 -0400

    Replace iteration through sources with bulk queries

--- R/fix.priority_id.by.source.R
+++ R/fix.priority_id.by.source.R
@@ -10,6 +10,8 @@ fix.priority_id.by.source <- function(toxval.db, source=NULL, subsource=NULL) {
 
   slist = source
   if(is.null(source)) slist = runQuery("select distinct source from toxval",toxval.db)[,1]
+  source_string = slist %>%
+    paste0(., collapse="', '")
 
   # Handle addition of subsource for queries
   query_addition = ""
@@ -18,14 +20,14 @@ fix.priority_id.by.source <- function(toxval.db, source=NULL, subsource=NULL) {
   }
 
   # Set appropriate priority ID in toxval using source_info table
-  for(source in slist) {
-    cat(source,"\n")
-    query = paste0("update toxval set priority_id=-1 where source like '",source,"'",query_addition)
-    runQuery(query,toxval.db)
-    pid = runQuery(paste0("select priority_id from source_info where source='",source,"'"),toxval.db)[1,1]
-    query = paste0("update toxval set priority_id=",pid," where source='",source,"'",query_addition)
-    runQuery(query,toxval.db)
-  }
+  cat("Updating priority ID for:", gsub("', '", ", ", source_string), "\n")
+  query = paste0("update toxval set priority_id=-1 where source in ('",source_string,"')",query_addition)
+  runQuery(query,toxval.db)
+  query = paste0("select priority_id from source_info where where source in ('",source_string,"')",query_addition)
+  pid = runQuery(query,toxval.db)[1,1]
+  query = paste0("update toxval set priority_id=",pid," where source in ('",source_string,"')",query_addition)
+  runQuery(query,toxval.db)
+
   x = runQuery("select distinct source from toxval where priority_id=-1",toxval.db)
   if(nrow(x)>0) print(x)
   cat("Number of sources with missing priority_id:",nrow(x),"\n")
