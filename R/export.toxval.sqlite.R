25a6d81c6795fb1946ae9a58693af153cc7942b9
Taylor Wall <wall.jonathan@epa.gov>
2024-07-08 10:28:52 -0400

    Improved console logging and recursive directory delete.

--- R/export.toxval.sqlite.R
+++ R/export.toxval.sqlite.R
@@ -13,20 +13,22 @@ export.toxval.sqlite <- function(toxval.db, source.db,
   if(dir.exists(export_repo)) {
     cat(paste0("Directory '", export_repo, "' already exists. Continuing will overwrite previous data.\n"))
     browser()
-    do.call(file.remove, list(list.files(new_directory, full.names = TRUE)))
+    unlink(export_repo, recursive = TRUE, force = TRUE)
+    cat(paste0("Overwriting directory: ", export_repo, "\n"))
   } else {
     cat(paste0("Initializing directory: ", export_repo, "\n"))
-    dir.create(export_repo)
   }
+  dir.create(export_repo)
 
   # Append repo information to output filenames
   tox_sqlite = paste0(export_repo, toxval_out)
   source_sqlite = paste0(export_repo, source_out)
 
-  # Get list of source tables to export
+  # Get list of source tables to export from main toxval table
   stage_table_list = runQuery("SELECT DISTINCT source_table FROM toxval", toxval.db) %>%
     dplyr::filter(!source_table %in% c("direct load", "direct_load")) %>%
     dplyr::pull(source_table) %>%
+    # Add additional source database tables to add
     c("source_chemical", "documents", "documents_records", "documents_lineage", "chemical_source_index")
 
   # Get list of toxval tables to export
@@ -37,13 +39,13 @@ export.toxval.sqlite <- function(toxval.db, source.db,
 
   # Write stage output to SQLite
   cat("Writing source tables to SQLite\n")
-  export_table_list_to_sqlite(stage_table_list, source.db, source_sqlite)
+  export_table_list_to_sqlite(table_list=stage_table_list, input_db=source.db, output_file=source_sqlite)
 
   # Write toxval output to SQLite
   cat("Writing toxval tables to SQLite\n")
-  export_table_list_to_sqlite(toxval_table_list, toxval.db, tox_sqlite)
+  export_table_list_to_sqlite(table_list=toxval_table_list, input_db=toxval.db, output_file=tox_sqlite)
 
-  cat("SQLite files writtent to", export_repo, "\n")
+  cat("SQLite files written to", export_repo, "\n")
 }
 
 #--------------------------------------------------------------------------------------
@@ -56,12 +58,12 @@ export_table_list_to_sqlite <- function(table_list, input_db, output_file) {
   output_db = RSQLite::dbConnect(RSQLite::SQLite(), output_file)
 
   for(table in table_list) {
+    cat("...pushing table ", table, " (", which(table %in% table_list), " of ",
+        length(table_list), " - ", format(Sys.time(), usetz = TRUE), ")", "\n")
     # Pull data to write from MySQL DB and write to SQLite
     data = runQuery(paste0("SELECT * FROM ", table), input_db)
-    dbWriteTable(output_db, table, data)
+    DBI::dbWriteTable(output_db, table, data)
   }
 
   RSQLite::dbDisconnect(output_db)
 }
-
-
