7696a776f0216686304da4c7df3a72a4036c40f9
Winter <Winter.Samuel@epa.gov>
2024-07-11 10:43:14 -0400

    Further updates to improve postprocessing efficiency

--- R/fix.study_group.R
+++ R/fix.study_group.R
@@ -12,8 +12,11 @@
 fix.study_group <- function(toxval.db, source=NULL, subsource=NULL, report.only=FALSE) {
   printCurrentFunction(toxval.db)
 
-  slist = runQuery("select distinct source from toxval",toxval.db)[,1]
-  if(!is.null(source)) slist = source
+  slist = source
+  if(is.null(source)) slist = runQuery("select distinct source from toxval",toxval.db)[,1]
+  source_string = slist %>%
+    paste0(., collapse="', '")
+
   # Handle addition of subsource for queries
   query_addition = NULL
   if(!is.null(subsource)) {
@@ -22,107 +25,105 @@ fix.study_group <- function(toxval.db, source=NULL, subsource=NULL, report.only=
 
   report.out = data.frame()
 
-  for(source in slist) {
-    cat(source,"\n")
-    if(!report.only){
-      # Reset to "-"
-      runQuery(paste0("update toxval set study_group='-' where source='",source,"'", query_addition), toxval.db)
-    }
-    # Query unique study fields
-    query = paste0("select a.toxval_id, a.source, a.subsource, a.dtxsid,c.common_name, a.toxval_units,  ",
-                   "a.study_type, a.exposure_route,a.exposure_method,a.exposure_form, ",
-                   "a.study_duration_value, a.study_duration_units, a.sex, a.lifestage, a.generation, a.year,",
-                   "a.strain, b.year as record_year, b.long_ref, b.title ",
-                   "from toxval a ",
-                   "left join record_source b on a.toxval_id=b.toxval_id ",
-                   "left join species c on a.species_id=c.species_id ",
-                   # Only use record_source entries from the source, not ToxVal team cataloging
-                   "where b.clowder_doc_id = '-' and b.record_source_level not in ('extraction', 'origin') ",
-                   "and a.source='",source,"'", query_addition)
+  if(!report.only){
+    # Reset to "-"
+    runQuery(paste0("update toxval set study_group='-' where source in ('",source_string,"')", query_addition), toxval.db)
+  }
+  # Query unique study fields
+  query = paste0("select a.toxval_id, a.source, a.subsource, a.dtxsid,c.common_name, a.toxval_units,  ",
+                 "a.study_type, a.exposure_route,a.exposure_method,a.exposure_form, ",
+                 "a.study_duration_value, a.study_duration_units, a.sex, a.lifestage, a.generation, a.year,",
+                 "a.strain, b.year as record_year, b.long_ref, b.title ",
+                 "from toxval a ",
+                 "left join record_source b on a.toxval_id=b.toxval_id ",
+                 "left join species c on a.species_id=c.species_id ",
+                 # Only use record_source entries from the source, not ToxVal team cataloging
+                 "where b.clowder_doc_id = '-' and b.record_source_level not in ('extraction', 'origin') ",
+                 "and a.source in ('",source_string,"')",
+                 gsub("subsource", "a.subsource", query_addition))
 
-    # Pull data
-    temp = runQuery(query,toxval.db)
-    # Hash to identify duplicate groups
-    temp.temp = temp %>%
-      tidyr::unite(hash_col, dplyr::all_of(sort(names(.)[!names(.) %in% c("toxval_id")])), sep="") %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(source_hash = paste0("ToxValhc_", digest::digest(hash_col, serialize = FALSE))) %>%
-      dplyr::ungroup()
+  # Pull data
+  temp = runQuery(query,toxval.db)
+  # Hash to identify duplicate groups
+  temp.temp = temp %>%
+    tidyr::unite(hash_col, dplyr::all_of(sort(names(.)[!names(.) %in% c("toxval_id")])), sep="") %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(source_hash = paste0("ToxValhc_", digest::digest(hash_col, serialize = FALSE))) %>%
+    dplyr::ungroup()
 
-    temp_sg = temp %>%
-      dplyr::mutate(source_hash = temp.temp$source_hash) %>%
-      dplyr::select(toxval_id, source, subsource, source_hash) %>%
-      # Collapse toxval_id for duplicate hashes
-      dplyr::group_by(dplyr::across(c(-toxval_id))) %>%
-      dplyr::summarise(toxval_id = toString(toxval_id)) %>%
+  temp_sg = temp %>%
+    dplyr::mutate(source_hash = temp.temp$source_hash) %>%
+    dplyr::select(toxval_id, source, subsource, source_hash) %>%
+    # Collapse toxval_id for duplicate hashes
+    dplyr::group_by(dplyr::across(c(-toxval_id))) %>%
+    dplyr::summarise(toxval_id = toString(toxval_id)) %>%
+    dplyr::ungroup() %>%
+    # Only account for those with duplicates
+    dplyr::filter(grepl(",", toxval_id))
+
+  if(nrow(temp_sg)){
+    temp_sg = temp_sg %>%
+      # Assign study group
+      dplyr::mutate(study_group = 1:n() %>%
+                      paste0(source, "_dup_", subsource, "_", .)) %>%
       dplyr::ungroup() %>%
-      # Only account for those with duplicates
-      dplyr::filter(grepl(",", toxval_id))
+      dplyr::select(-source_hash) %>%
+      # Separate collapse toxval_id groups
+      tidyr::separate_rows(toxval_id, sep=", ") %>%
+      dplyr::mutate(toxval_id = as.numeric(toxval_id))
+  } else {
+    # Set up empty duplicate study_group dataframe
+    temp_sg = temp %>%
+      dplyr::mutate(study_group = NA) %>%
+      .[0,]
+  }
 
-    if(nrow(temp_sg)){
-      temp_sg = temp_sg %>%
-        # Assign study group
-        dplyr::mutate(study_group = 1:n() %>%
-                        paste0(source, "_dup_", subsource, "_", .)) %>%
-        dplyr::ungroup() %>%
-        dplyr::select(-source_hash) %>%
-        # Separate collapse toxval_id groups
-        tidyr::separate_rows(toxval_id, sep=", ") %>%
-        dplyr::mutate(toxval_id = as.numeric(toxval_id))
-    } else {
-      # Set up empty duplicate study_group dataframe
-      temp_sg = temp %>%
-        dplyr::mutate(study_group = NA) %>%
-        .[0,]
-    }
+  # Check/report if any duplicates present
+  nr = nrow(temp)
+  nsg = length(unique(temp_sg$study_group)) + length(temp$toxval_id[!temp$toxval_id %in% temp_sg$toxval_id])
+  cat("  nrow:",nr," unique values:",nsg,"\n")
+  # Set default study group to toxval_id
+  if(!report.only){
+    query = paste0("update toxval set study_group=CONCAT(source,':',toxval_id,':',sex,':',generation,lifestage) ",
+                   "where source in ('",source_string,"')'",
+                   query_addition)
+    runQuery(query,toxval.db)
+  } else {
+    query = paste0("SELECT toxval_id, CONCAT(source,':',toxval_id,':',sex,':',generation,lifestage) as study_group from toxval ",
+                   "where source in ('",source_string,"')",
+                   query_addition)
+    src.report.out = runQuery(query,toxval.db) %>%
+      # Filter out dups
+      dplyr::filter(!toxval_id %in% temp_sg$toxval_id)
+  }
 
-    # Check/report if any duplicates present
-    nr = nrow(temp)
-    nsg = length(unique(temp_sg$study_group)) + length(temp$toxval_id[!temp$toxval_id %in% temp_sg$toxval_id])
-    cat("  nrow:",nr," unique values:",nsg,"\n")
-    # Set default study group to toxval_id
+  # If duplicate groups, set to generated study group
+  if(nsg!=nr) {
+    cat("   Number of dups:", length(unique(temp_sg$study_group)),"\n")
     if(!report.only){
-      query = paste0("update toxval set study_group=CONCAT(source,':',toxval_id,':',sex,':',generation,lifestage) where source='",
-                     source,"'",
-                     query_addition)
-      runQuery(query,toxval.db)
+      # Query to inner join and make updates with mw dataframe (temp table added/dropped)
+      updateQuery = paste0("UPDATE toxval a INNER JOIN z_updated_df b ",
+                           "ON (a.toxval_id = b.toxval_id) SET a.study_group = CONCAT(b.study_group,':',a.sex,':',a.generation,a.lifestage) ",
+                           "WHERE a.study_group IS NOT NULL")
+      # Run update query
+      runUpdate(table="toxval", updateQuery=updateQuery, updated_df=temp_sg, db=toxval.db)
     } else {
-      query = paste0("SELECT toxval_id, CONCAT(source,':',toxval_id,':',sex,':',generation,lifestage) as study_group from toxval where source='",
-                     source,"'",
-                     query_addition)
-      src.report.out = runQuery(query,toxval.db) %>%
-        # Filter out dups
-        dplyr::filter(!toxval_id %in% temp_sg$toxval_id)
-    }
+      query = paste0("SELECT toxval_id, CONCAT(sex,':',generation,lifestage) as study_group_addition FROM toxval WHERE toxval_id in (",
+                     toString(temp_sg$toxval_id),")")
+      src.dups = runQuery(query,toxval.db)
 
-    # If duplicate groups, set to generated study group
-    if(nsg!=nr) {
-      cat("   Number of dups:", length(unique(temp_sg$study_group)),"\n")
-      if(!report.only){
-        # Query to inner join and make updates with mw dataframe (temp table added/dropped)
-        updateQuery = paste0("UPDATE toxval a INNER JOIN z_updated_df b ",
-                             "ON (a.toxval_id = b.toxval_id) SET a.study_group = CONCAT(b.study_group,':',a.sex,':',a.generation,a.lifestage) ",
-                             "WHERE a.study_group IS NOT NULL")
-        # Run update query
-        runUpdate(table="toxval", updateQuery=updateQuery, updated_df=temp_sg, db=toxval.db)
-      } else {
-        query = paste0("SELECT toxval_id, CONCAT(sex,':',generation,lifestage) as study_group_addition FROM toxval WHERE toxval_id in (",
-                       toString(temp_sg$toxval_id),")")
-        src.dups = runQuery(query,toxval.db)
-
-        src.report.out = temp_sg %>%
-          dplyr::left_join(src.dups,
-                           by="toxval_id") %>%
-          tidyr::unite(col="study_group", study_group, study_group_addition, sep = ":", na.rm=TRUE) %>%
-          # Add dups to report
-          dplyr::bind_rows(src.report.out, .)
-      }
+      src.report.out = temp_sg %>%
+        dplyr::left_join(src.dups,
+                         by="toxval_id") %>%
+        tidyr::unite(col="study_group", study_group, study_group_addition, sep = ":", na.rm=TRUE) %>%
+        # Add dups to report
+        dplyr::bind_rows(src.report.out, .)
     }
-    # Bind source reports
-    if(report.only){
-      report.out = report.out %>%
-        dplyr::bind_rows(src.report.out)
-    }
+  }
+  # Bind source reports
+  if(report.only){
+    report.out = report.out %>%
+      dplyr::bind_rows(src.report.out)
   }
 
   # Return report
