8e3e196326d1914f9b0dd556a7c9a792d289dec6
Taylor Wall <wall.jonathan@epa.gov>
2024-12-11 14:28:37 -0500

    HotFix: HAWC PFAS 430 missing subsource_url

--- R/toxval.load.hawc_pfas_430.R
+++ R/toxval.load.hawc_pfas_430.R
@@ -180,7 +180,7 @@ toxval.load.hawc_pfas_430 <- function(toxval.db, source.db, log=FALSE, remove_nu
   refs = dplyr::distinct(refs)
   res$datestamp = Sys.Date()
   res$source_table = source_table
-  res$subsource_url = "-"
+  # res$subsource_url = "-"
   res$details_text = paste(source,"Details")
   runInsertTable(res, "toxval", toxval.db, verbose)
   print(paste0("Dimensions of source data pushed to toxval: ", toString(dim(res))))
