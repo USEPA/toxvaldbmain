4ffd49a33c8c7a9c6f933f051700b0c4fb115390
Wall <Wall.Taylor@epa.gov>
2025-04-03 15:35:42 -0400

    Updated qc_stats generation by supersource and updated supplemental table generation.

--- R/release/get_supplemental_tables.R
+++ R/release/get_supplemental_tables.R
@@ -1,13 +1,53 @@
 get_supplemental_tables <- function(toxval.db, source.db){
 
+  chem_src_index = runQuery("SELECT distinct chemprefix, curation_type FROM chemical_source_index",
+                            source.db)
+
+  source_catalog = runQuery(paste0("SELECT distinct supersource, source, source_table, chemical_id FROM toxval"),
+                            toxval.db) %>%
+    tidyr::separate(chemical_id, into = c("chemprefix", "chemical_hash")) %>%
+    dplyr::select(-chemical_hash) %>%
+    dplyr::distinct() %>%
+    dplyr::left_join(chem_src_index,
+                     by = "chemprefix")
+
+
+
   supp_tables = list()
+
+  supp_tables[["README"]] =
+    data.frame(
+      `Suplemental Table` = c(
+        "Table S1.",
+        "Table S2.",
+        "Table S3.",
+        "Table S4a.",
+        "Table S4b.",
+        "Table S4c.",
+        "Table S5.",
+        "Table S6.",
+        "Table S7."
+      ),
+      Description = c(
+        "Factors used to convert food or water exposure in ppm to mg/kg-day.",
+        "Source name acronyms.",
+        "Effect type (toxval_type) acronyms.",
+        "In vivo Effect type (toxval_type) by source 'in vivo'.",
+        "Toxicity Values Effect type (toxval_type) by source.",
+        "Exposure Guidelines Effect type (toxval_type) by source.",
+        "QC summary table. Sources and QC Level 1 Sampling Counts in ToxValDB v9.6.1.",
+        'Sources and counts with the QC pass status included in ToxValDB v9.6.1. Colors indicate increasing values from low to high as red to green.',
+        'Sources and counts with the QC fail status included in ToxValDB v9.6.1. Colors indicate increasing values from low to high as green to red. Note the color reversal for "fail" statuses.'
+
+      )
+    )
 
   supp_tables[["Table S1"]] = readxl::read_xlsx(paste0(toxval.config()$datapath,
                                                        "dictionary/ppm to mgkgday by animal.xlsx"))
 
   supp_tables[["Table S2"]] = runQuery("SELECT * FROM source_info",
                                        toxval.db) %>%
-    dplyr::select(supersource, full_name = supersource_full_name) %>%
+    dplyr::select(source = supersource, full_name = supersource_full_name) %>%
     dplyr::distinct()
 
   supp_tables[["Table S3"]] = runQuery(paste0("select distinct toxval_type_category as toxval_type, fullname as full_name ",
@@ -34,6 +74,7 @@ get_supplemental_tables <- function(toxval.db, source.db){
                                        "Media Exposure Guidelines") ~ "exposure guideline(s)",
       TRUE ~ NA
     )) %>%
+    dplyr::filter(!is.na(group)) %>%
     dplyr::group_split(group) %>%
     lapply(., function(df){
       if(unique(df$group) == "in vivo"){
@@ -52,8 +93,7 @@ get_supplemental_tables <- function(toxval.db, source.db){
     })
 
   # https://stackoverflow.com/questions/8091303/simultaneously-merge-multiple-data-frames-in-a-list
-  source_toxval_type = c(list(supp_tables[["Table S2"]] %>%
-                                dplyr::select(source = supersource)),
+  source_toxval_type = c(list(supp_tables[["Table S2"]]),
                          source_toxval_type
   ) %>%
     purrr::reduce(left_join, by = "source") %>%
@@ -75,5 +115,68 @@ get_supplemental_tables <- function(toxval.db, source.db){
   supp_tables[["Table S4c"]] = source_toxval_type %>%
     dplyr::select(source, `exposure guideline(s)`) %>%
     dplyr::distinct()
+
+  source_rec_n = runQuery(paste0("select supersource, source_table, count(*) as 'total records' ",
+                                 "from toxval group by supersource, source_table"),
+                          toxval.db)
+
+  # Load in manually curated sampling summary dict
+  qc_sampling_summary = readxl::read_xlsx(paste0(toxval.config()$datapath,
+                                                 "release_files/toxval_qc_sampling_summary_dict_v96_1.xlsx"))
+
+  supp_tables[["Table S5"]] = source_rec_n %>%
+    dplyr::left_join(source_catalog,
+                     by = c("supersource", "source_table")) %>%
+    dplyr::select(source = supersource, `total records`,
+                  `source table` = source_table, `curation type` = curation_type) %>%
+    dplyr::left_join(qc_sampling_summary,
+                     by = c("source", "source table"))
+
+  qc_status_summary = toxval.summary.stats(toxval.db, summ_level = "supersource")
+
+  supp_tables[["Table S6"]] = qc_status_summary %>%
+    dplyr::select(source = supersource, chemicals, `total records`, pass, `not determined`, `pass %` = `pass percent`)
+
+  qc_fail = qc_status_summary %>%
+    dplyr::select(-c(chemicals, pass, `not determined`, `pass percent`)) %>%
+    dplyr::rename(source = supersource)
+
+  # Rename fail columns and set as caption dynamically
+  qc_fail = qc_fail %>%
+    tidyr::pivot_longer(cols = -c(source, `total records`))
+
+  # Get caption list
+  fail_captions = qc_fail %>%
+    dplyr::select(name) %>%
+    unique() %>%
+    dplyr::mutate(name_sym = rep_len(letters, nrow(.)))
+
+  # Map caption list back
+  qc_fail = qc_fail %>%
+    dplyr::left_join(fail_captions, by = "name") %>%
+    dplyr::select(-name) %>%
+    tidyr::pivot_wider(id_cols = c(source, `total records`),
+                       names_from = "name_sym")
+
+  # Add caption notes
+  qc_fail_caption = data.frame(
+    "total records" = c(
+      "i. QC fail status tags listed as programmatically applied during load to ToxValDB toxval table, except for the “fail” status which is from a manually reviewed record.",
+      "ii. Note, an individual record can have multiple fail tags (e.g., 'fail: species out of scope; Eco data out of scope for ToxValDB' would be tag 'e' and tag 'h')",
+      "iii. Failure column tags:"
+    )
+  ) %>%
+    dplyr::bind_rows(
+      fail_captions %>%
+        tidyr::unite(col = "total.records", name_sym, name, sep = " - ")
+    ) %>%
+    dplyr::rename(`total records` = `total.records`)
+
+  supp_tables[["Table S7"]] = qc_fail %>%
+    dplyr::mutate(`total records` = as.character(`total records`)) %>%
+    dplyr::bind_rows(qc_fail_caption)
+
+  writexl::write_xlsx(supp_tables,
+                      paste0(toxval.config()$datapath, "/release_files/", toxval.db," Supplemental Tables_",Sys.Date(),".xlsx"))
 
 }
--- R/toxval.summary.stats.R
+++ R/toxval.summary.stats.R
@@ -3,41 +3,54 @@
 #'
 #' @param toxval.db The version of toxval into which the tables are loaded.
 #' @param export Boolean whether to export a file. Default FALSE.
+#' @param summ_level String of "source" or "supersource" how to group the summary
 #' @export
 #' @return DataFrame of record qc_status summary by source.
 #'
 #--------------------------------------------------------------------------------------
-toxval.summary.stats <- function(toxval.db, export=FALSE) {
+toxval.summary.stats <- function(toxval.db, export=FALSE, summ_level = "source") {
   # printCurrentFunction(toxval.db)
 
   # Query summarize all source qc statuses in the database
-  query = paste0("SELECT source, qc_status, count(*) as n FROM toxval GROUP BY source, qc_status")
+  query = paste0("SELECT ", summ_level,", qc_status, count(*) as n FROM toxval GROUP BY ", summ_level, ", qc_status")
   qc_stats_db = runQuery(query, toxval.db)
 
   # Get qc_status fail logs
   outDir = paste0(toxval.config()$datapath, "export_qc_fail/", toxval.db)
   qc_stats_fail = list.files(outDir, full.names = TRUE)
   qc_stats_fail = lapply(qc_stats_fail, readxl::read_xlsx) %>%
-    dplyr::bind_rows()
+    dplyr::bind_rows() %>%
+    dplyr::select(dplyr::any_of(c(summ_level, "chemical_id", "qc_status")))
+
+  # if(summ_level == "supersource"){
+  #   supersource_map = runQuery("SELECT distinct supersource, source FROM toxval",
+  #                              toxval.db)
+  #
+  #   # Add supersource column back in
+  #   qc_stats_fail = qc_stats_fail %>%
+  #     dplyr::left_join(supersource_map,
+  #                      by = "source") %>%
+  #     dplyr::select(supersource, dplyr::everything())
+  # }
 
   # Combine and sum counts
-  res_combined = runQuery("SELECT source, chemical_id FROM toxval",
+  res_combined = runQuery(paste0("SELECT ", summ_level,", chemical_id FROM toxval"),
                  toxval.db) %>%
     dplyr::bind_rows(qc_stats_fail %>%
-                       dplyr::select(source, chemical_id))
+                       dplyr::select(dplyr::any_of(c(summ_level, "chemical_id"))))
 
   res = res_combined %>%
-    dplyr::count(source, name = "total records") %>%
+    dplyr::count(dplyr::across(summ_level), name = "total records") %>%
     dplyr::left_join(res_combined %>%
                        dplyr::distinct() %>%
-                       dplyr::count(source, name = "chemicals"),
-                     by = "source") %>%
-    dplyr::arrange(source) %>%
-    dplyr::select(source, chemicals, `total records`)
+                       dplyr::count(dplyr::across(summ_level), name = "chemicals"),
+                     by = summ_level) %>%
+    dplyr::arrange(dplyr::across(summ_level)) %>%
+    dplyr::select(dplyr::any_of(c(summ_level, "chemicals", "total records")))
 
   # Summarize qc_status for fails
   qc_stats_fail = qc_stats_fail %>%
-    dplyr::count(source, qc_status)
+    dplyr::count(dplyr::across(c(summ_level, "qc_status")))
 
   # Combine and summarize
   qc_stats = qc_stats_db %>%
@@ -56,24 +69,24 @@ toxval.summary.stats <- function(toxval.db, export=FALSE) {
     dplyr::mutate(qc_status = qc_status %>%
                     stringr::str_squish() %>%
                     gsub(": ", ":", .)) %>%
-    dplyr::group_by(source, qc_status) %>%
+    dplyr::group_by(dplyr::across(c(summ_level, "qc_status"))) %>%
     # Summarize split groups
     dplyr::summarise(n = sum(n)) %>%
     dplyr::ungroup() %>%
     # Convert to column headers
-    tidyr::pivot_wider(id_cols = c("source"), names_from = qc_status, values_from = n)
+    tidyr::pivot_wider(id_cols = c(summ_level), names_from = qc_status, values_from = n)
 
   # Combine
   out = res %>%
     dplyr::left_join(qc_stats,
-                     by="source") %>%
+                     by=summ_level) %>%
     # Summarize pass percentage
     dplyr::mutate(dplyr::across(dplyr::everything(), ~replace(., is.na(.), 0)),
-                  `pass percent` = round(((pass + `not determined`)/ `total records`) * 100, 3)) %>%
+                  `pass percent` = round(((pass + `not determined`)/ `total records`) * 100, 1)) %>%
     # Reorder columns
     dplyr::select(order(colnames(.), decreasing = TRUE)) %>%
-    dplyr::select(source, chemicals, `total records`, pass,
-                  `not determined`, `pass percent`, dplyr::everything())
+    dplyr::select(dplyr::all_of(c(summ_level, "chemicals", "total records", "pass",
+                  "not determined", "pass percent")), dplyr::everything())
 
   if(export){
     file = paste0(toxval.config()$datapath,"export/source_count ",Sys.Date(),".xlsx")
