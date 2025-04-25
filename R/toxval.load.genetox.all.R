65011d9b4970db23e92e09ad1384b4908535f412
Wall <Wall.Taylor@epa.gov>
2025-04-25 12:47:18 -0400

    Update to use 2024-04-01 data.

--- R/toxval.load.genetox.all.R
+++ R/toxval.load.genetox.all.R
@@ -6,19 +6,18 @@
 #' @param verbose If TRUE output debug information
 #' @export
 #--------------------------------------------------------------------------------------
-toxval.load.genetox.all <- function(toxval.db, source.db, sys.date="2021-09-10", verbose=FALSE) {
+toxval.load.genetox.all <- function(toxval.db, source.db, sys.date="2024-04-01", verbose=FALSE) {
   printCurrentFunction(toxval.db)
 
   # Handle genetox_details
-  runQuery("delete from genetox_details", toxval.db)
-  file <- paste0(toxval.config()$datapath,"genetox/dataprep/combined_genetox_standard_",sys.date,".xlsx")
+  runQuery("DROP TABLE IF EXISTS genetox_details", toxval.db)
+  file <- paste0(toxval.config()$datapath,"genetox/combined_genetox_standard_",sys.date,".xlsx")
   res0 = readxl::read_xlsx(file)
 
   # Drop entries missing CASRN (logic brought over from old script)
   res = res0 %>%
     # Drop entries missing CASRN (logic brought over from old script)
     tidyr::drop_na(casrn) %>%
-
     dplyr::mutate(
       # Clean names
       name = name %>%
@@ -29,13 +28,11 @@ toxval.load.genetox.all <- function(toxval.db, source.db, sys.date="2021-09-10",
       # Clean species
       species = species %>%
         fix.replace.unicode() %>%
-        tolower() %>%
         stringr::str_squish(),
 
       # Clean strain
       strain = strain %>%
         fix.replace.unicode() %>%
-        tolower() %>%
         stringr::str_squish(),
 
       # Clean cytotoxicity
@@ -63,8 +60,11 @@ toxval.load.genetox.all <- function(toxval.db, source.db, sys.date="2021-09-10",
       # Clean glp
       glp = glp %>%
         fix.replace.unicode() %>%
-        stringr::str_squish()
+        stringr::str_squish(),
+      genetox_details_id = 1:dplyr::n(),
+      clowder_doc_id = "https://clowder.edap-cluster.com/files/680b9adfe4b096bca880a4ad"
     ) %>%
+    dplyr::select(-dtxsid) %>%
     # Clean name/casrn and set chemical_id in source_chemical table
     source_chemical.extra(toxval.db, source.db, ., "Genetox Details")
 
@@ -74,9 +74,12 @@ toxval.load.genetox.all <- function(toxval.db, source.db, sys.date="2021-09-10",
 
   # Ensure only appropriate columns are present (old logic - untouched)
   n1 = names(res2)
-  n2 = runQuery("desc genetox_details",toxval.db)[,1]
-  n3 = n1[is.element(n1,n2)]
-  res2 = res2[,n3]
+  n2 = runQuery("desc genetox_summary_20210910",toxval.db)[,1]
+  missing_n = n2[!n2 %in% n1]
+  # n3 = n1[is.element(n1,n2)]
+  # res2 = res2[,n3]
+  # Fill blank fields
+  res2[, missing_n] <- "-"
 
   # Send genetox_details data to ToxVal
   runInsertTable(res2, "genetox_details", toxval.db,verbose)
@@ -84,14 +87,14 @@ toxval.load.genetox.all <- function(toxval.db, source.db, sys.date="2021-09-10",
   #--------------------------------------------------------------------------------------
 
   # Handle genetox_summary
-  runQuery("delete from genetox_summary", toxval.db)
-  file <- paste0(toxval.config()$datapath,"genetox/dataprep/genetox_summary_",sys.date,".xlsx")
+  runQuery("DROP TABLE IF EXISTS genetox_summary", toxval.db)
+  file <- paste0(toxval.config()$datapath,"genetox/genetox_summary_",sys.date,".xlsx")
   res0 = readxl::read_xlsx(file, col_types = "text")
 
   res = res0 %>%
     # Drop entries with NA casrn (updated old logic)
     tidyr::drop_na(casrn) %>%
-
+    dplyr::select(-dtxsid) %>%
     dplyr::mutate(
       # Clean chemical name
       name = name %>%
@@ -103,7 +106,9 @@ toxval.load.genetox.all <- function(toxval.db, source.db, sys.date="2021-09-10",
       # Set appropriate columns to numeric type
       reports_pos = as.numeric(reports_pos),
       reports_neg = as.numeric(reports_neg),
-      reports_other = as.numeric(reports_other)
+      reports_other = as.numeric(reports_other),
+      genetox_summary_id = 1:dplyr::n(),
+      clowder_doc_id = "https://clowder.edap-cluster.com/files/680b9b14e4b096bca880a4c7"
     ) %>%
 
     # Clean name/casrn and set chemical_id in source_chemical table
@@ -112,6 +117,13 @@ toxval.load.genetox.all <- function(toxval.db, source.db, sys.date="2021-09-10",
   # Select columns of interest (old logic moved over)
   res2 = res %>%
     dplyr::select(-c(casrn,name,chemical_index))
+
+  # Ensure only appropriate columns are present (old logic - untouched)
+  n1 = names(res2)
+  n2 = runQuery("desc genetox_summary_20210910",toxval.db)[,1]
+  missing_n = n2[!n2 %in% n1]
+  # Fill blank fields
+  res2[, missing_n] <- "-"
 
   # Send genetox_summary data to ToxVal
   runInsertTable(res2, "genetox_summary", toxval.db)
